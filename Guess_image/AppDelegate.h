1413921c6ec0f94f53c952648b573723f4bb518d
Leohongyi <wenhualiu.liu@gmail.com>
2015-06-01 22:32:12 -0400

    Initial Commit

---
+++ Guess_image/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guess_image
+//
+//  Created by apple on 6/1/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Guess_image/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Guess_image
+//
+//  Created by apple on 6/1/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
